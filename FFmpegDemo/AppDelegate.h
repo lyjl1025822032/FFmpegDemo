1c450c6941c3bd6cf16662d85b044373078ec0a1
lyjl1025822032 <m18272733524@163.com>
2017-10-12 15:46:09 +0800

    Initial Commit

---
+++ FFmpegDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFmpegDemo
+//
+//  Created by 王智垚 on 2017/10/12.
+//  Copyright © 2017年 王智垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFmpegDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpegDemo
+//
+//  Created by 王智垚 on 2017/10/12.
+//  Copyright © 2017年 王智垚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
